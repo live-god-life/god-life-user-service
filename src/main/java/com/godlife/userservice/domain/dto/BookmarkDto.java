9d3d2cf77a0d21af0d4a63ed0fd3dc02c555f8ae
ss-hoon <lshoon1128@naver.com>
2022-11-12 17:02:25 +0900

    rename: FeedBookmarkDto -> BookmarkDto

--- src/main/java/com/godlife/userservice/domain/dto/BookmarkDto.java
+++ src/main/java/com/godlife/userservice/domain/dto/BookmarkDto.java
@@ -1,19 +1,13 @@
 package com.godlife.userservice.domain.dto;
 
-import com.godlife.userservice.domain.entity.Users;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class BookmarkDto {
-
-    /** 북마크 번호 */
-    private Long bookmarkId;
-
-    /** 회원 아이디 */
-    private Users user;
 
     /** 피드 아이디 */
     private Long feedId;
+
+    /** 북마크 상태 */
+    private boolean bookmarkStatus;
 }
