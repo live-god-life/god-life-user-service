73b0090bd36b4fcdf9ae00136d2a4f57891593a4
ss-hoon <lshoon1128@naver.com>
2022-11-19 11:59:06 +0900

    fix: 회원 정보가 없을 경우 api response code 변경

--- src/main/java/com/godlife/userservice/response/ResponseCode.java
+++ src/main/java/com/godlife/userservice/response/ResponseCode.java
@@ -19,8 +19,9 @@ public enum ResponseCode {
 	// 실패 코드
 	INVALID_PARAMETER("error", 400, "올바른 정보가 아닙니다.", HttpStatus.BAD_REQUEST),                   // 파라미터 오류
 	DUPLICATE_NICKNAME("error", 402, "중복된 닉네임입니다.", HttpStatus.CONFLICT),                       // 닉네임 중복
-	DUPLICATE_USER("error", 402, "이미 가입된 회원입니다.", HttpStatus.CONFLICT),                         // 회원 중복
-	NOT_FOUND_USER("error", 401, "프로필 정보 조회를 실패했습니다.", HttpStatus.NOT_FOUND);                 // 프로필 조회 실패
+	DUPLICATE_USER("error", 402, "이미 가입된 회원입니다.", HttpStatus.CONFLICT),                        // 회원 중복
+	NOT_FOUND_USER("error", 401, "프로필 정보 조회를 실패했습니다.", HttpStatus.OK);                		// 프로필 조회 실패
+
 
 	/** 상태 (success / error) */
 	private String status;
